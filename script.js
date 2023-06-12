31eae27592cbc5041cd878d2286667230cbf461a
quodlibetEns <91636891+quodlibetEns@users.noreply.github.com>
2023-06-12 15:07:21 +0100

    Fix bug where previous results stay on screen

--- script.js
+++ script.js
@@ -101,7 +101,9 @@ async function getResults (selectId) {
 
     const result = document.getElementById('result');
 
-    result.innerHTML += newContent; //add new html onto end of ucrrent result items
+    const scrollButtonHtml = `<div id="scroll-to-top" onclick="scrollToTop()">&#8593;</div>`;
+
+    result.innerHTML = scrollButtonHtml + newContent;
     result.style.display = "block"; //make results area visible once generated
     result.scrollIntoView(true); //true is value to alignToTop
 
